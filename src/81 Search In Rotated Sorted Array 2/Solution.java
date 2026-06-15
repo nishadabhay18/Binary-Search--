3f7d94cc4af4a86153ea061567bfb14102eca337
nishadabhay18 <abhayn927@gmail.com>
2026-06-15 20:28:29 +0530

    init commit

---
+++ src/81 Search In Rotated Sorted Array 2/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    // TC-> O(n) SC-> O(1)
+    public boolean search(int[]arr, int target) {
+        int n = arr.length;
+        int st = 0, end = n-1;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            if(arr[mid] == target) return true;
+            else if(arr[st] == arr[mid] && arr[mid] == arr[end]){
+                st++;
+                end--;
+            }
+            else if(arr[mid] <= arr[end]){
+                if(target > arr[mid] && target <= arr[end]) st = mid+1;
+                else end = mid-1;
+            }else{
+                if(target >= arr[st] && target < arr[mid]) end = mid-1;
+                else st = mid+1;
+            }
+        }
+        return false;
+    }
+}
